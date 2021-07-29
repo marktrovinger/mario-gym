1e300dbfec8116628a6e47c89f8cd3ecac368045
Mark Trovinger <mark.trovinger@gmail.com>
2021-07-29 18:22:22 -0400

    added wandb support

--- mario-testing-sb3.py
+++ mario-testing-sb3.py
@@ -1,28 +1,49 @@
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
+import gym
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-from stable_baselines3 import DQN
+from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.evaluation import evaluate_policy
 from preprocess import SkipFrame, GrayScaleObservation, ResizeObservation, FrameStack
+from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
+from wandb.integration.sb3 import WandbCallback
+import wandb
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
+config = {"policy_type":"MlpPolicy", "total_timesteps": 50000}
+run = wandb.init(
+    project="mario",
+    config=config,
+    sync_tensorboard=True,
+    monitor_gym = True,
+    save_code = True
+ )
 
-env = SkipFrame(env, skip=4)
-env = GrayScaleObservation(env)
-env = ResizeObservation(env, shape=84)
-env = FrameStack(env, num_stack=4)
 
-done = True
+def make_env():
+    env = gym_super_mario_bros.make('SuperMarioBros-v0')
+    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    env = SkipFrame(env, skip=4)
+    env = GrayScaleObservation(env)
+    env = ResizeObservation(env, shape=84)
+    env = FrameStack(env, num_stack=4)
+    env = Monitor(env)
+    return env
+    
+env = DummyVecEnv([make_env])
 
-model = DQN('CnnPolicy', env, verbose=1)
-model.learn(total_timesteps=int(40000))
-model.save("dqn_mario_downsample")
+env = VecVideoRecorder(env, "videos",
+    record_video_trigger=lambda x: x % 2000 == 0, video_length=200)
 
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 
-obs = env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
+
+model = PPO(config['policy_type'], env, verbose=1,
+    tensorboard_log=f"runs/{run.id}")
+model.learn(
+    total_timesteps=config['total_timesteps'], 
+    callback=WandbCallback(
+        gradient_save_freq=100,
+        model_save_freq=1000,
+        model_save_path=f"models/{run.id}",
+    ),
+)
